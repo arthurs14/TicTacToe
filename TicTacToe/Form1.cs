e0f66f53babee4d0bae8845cc234998c2cc6fba9
Arthur <arthurcs426@gmail.com>
2024-01-22 12:59:17 -0800

    added comments to functins

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -1,5 +1,8 @@
 namespace TicTacToe
 {
+    // <summary>
+    // Form class that handles user clicks and CPU move
+    // </summary>
     public partial class Form1 : Form
     {
         public enum Player
@@ -19,6 +22,10 @@ namespace TicTacToe
             RestartGame();
         }
 
+        // <summary>
+        // Checks which buttons are still available and randomly chooses one
+        // Also checks if game has been won
+        // </summary>
         private void CPUMove(object sender, EventArgs e)
         {
             if (buttons?.Count > 0)
@@ -34,6 +41,10 @@ namespace TicTacToe
             }
         }
 
+        // <summary>
+        // Gets the button user clicked and updates.
+        // Also checks if game has been won
+        // </summary>
         private void PlayerClickButton(object sender, EventArgs e)
         {
             var button = (Button)sender;
@@ -46,11 +57,18 @@ namespace TicTacToe
             CPUTimer.Start();
         }
 
+        // <summary>
+        // Reset game via reset button from form
+        // </summary>
         private void RestartGame(object sender, EventArgs e)
         {
             RestartGame();
         }
 
+        // <summary>
+        // Checks to see if player or cpu has won the game.
+        // Updates Player and CPU scores accordingly
+        // </summary>
         private void CheckGame() 
         {
             if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
@@ -87,6 +105,9 @@ namespace TicTacToe
             }
         }
 
+        // <summary>
+        // Restarts game by reseting the text and making them enabled again
+        // </summary>
         private void RestartGame()
         {
             buttons = new List<Button> { button1, button2, button3, button4,
